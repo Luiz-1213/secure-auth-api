39a454cd76e761debd1a13a001562b234fbb6294
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-06-27 14:00:17 -0300

    feat(config): add OAuth-related environment variables

--- src/application/config/env.ts
+++ src/application/config/env.ts
@@ -1,12 +1,24 @@
 import z from 'zod';
 
-const schema = z.string();
-const result = schema.safeParse(process.env.JWT_SECRET);
-
-if (result.error) {
-  throw new Error('JWT secret is required!');
-}
+const schema = z.object({
+  JWT_SECRET: z.string().min(1),
+  GOOGLE_CLIENT_ID: z.string().min(1),
+  GOOGLE_CLIENT_SECRET: z.string().min(1),
+  GITHUB_CLIENT_ID: z.string().min(1),
+  GITHUB_CLIENT_SECRET: z.string().min(1),
+});
 
 export const env = {
-  jwtSecret: process.env.JWT_SECRET!,
+  JWT_SECRET: process.env.JWT_SECRET!,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 };
+
+const result = schema.safeParse(env);
+
+if (result.error) {
+  const pathError = result.error.issues[0].path;
+  throw new Error(`This env ${pathError} is required`);
+}
