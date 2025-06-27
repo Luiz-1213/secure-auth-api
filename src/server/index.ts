a50f4f89bb70ee8fe332730d81daaafb3e41daaa
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-06-27 14:02:57 -0300

    feat(server): add CORS middleware to allow cross-origin requests

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,31 @@
-import express from 'express';
+import express, { NextFunction, Response } from 'express';
 
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { makeSignWithGithubController } from '../factories/makeSignWithGithubController';
 
 import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+const cors = (request, response: Response, next: NextFunction) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
+
+  next();
+};
+
 app.use(express.json());
+
+app.use(cors);
 
 app.post('/sign-up', routeAdapter(makeSignUpController()));
 app.post('/sign-in', routeAdapter(makeSignInController()));
+app.post('/auth/google', routeAdapter(makeSignInController()));
+app.post('/auth/github', routeAdapter(makeSignWithGithubController()));
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
