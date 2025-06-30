89c755d6f9a4e4596fcfa156dec1f7746e5e930b
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-06-30 13:24:45 -0300

    feat(server): add routes for Google, GitHub OAuth and token refresh

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,17 @@
-import express, { NextFunction, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
+import { makeRefreshTokenController } from '../factories/makeRefreshTokenController';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
 import { makeSignWithGithubController } from '../factories/makeSignWithGithubController';
+import { makeSignWithGoogleController } from '../factories/makeSignWithGoogleController';
 
 import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-const cors = (request, response: Response, next: NextFunction) => {
+const cors = (request: Request, response: Response, next: NextFunction) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
@@ -24,7 +26,9 @@ app.use(cors);
 
 app.post('/sign-up', routeAdapter(makeSignUpController()));
 app.post('/sign-in', routeAdapter(makeSignInController()));
-app.post('/auth/google', routeAdapter(makeSignInController()));
+app.post('/revoke', routeAdapter(makeSignInController()));
+app.post('/refresh-token', routeAdapter(makeRefreshTokenController()));
+app.post('/auth/google', routeAdapter(makeSignWithGoogleController()));
 app.post('/auth/github', routeAdapter(makeSignWithGithubController()));
 
 app.listen(port, () => {
